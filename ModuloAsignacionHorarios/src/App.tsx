2bf62c4b3e55a1841defc020ea9114cb30586869
EfrenAcevedo <joseefrenacevedo3@gmail.com>
2026-01-16 10:10:40 -0600

    se agrego pagina de error

--- ModuloAsignacionHorarios/src/App.tsx
+++ ModuloAsignacionHorarios/src/App.tsx
@@ -2,27 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //@ts-ignore
 import ExcelManager from "./Components/ExcelManager.jsx";
 //@ts-ignore
-import Navbar from "./Components/Navbar.jsx";
-//@ts-ignore
 import Home from "./Components/Generic/Home.jsx";
 //@ts-ignore
-import Footer from "./Components/Generic/Footer.jsx";
+import NotFound from "./Components/Generic/NotFound";
+//@ts-ignore
+import MainLayout from "./Components/Generic/MainLayout";
 
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cargadatos" element={<ExcelManager />} />
-        <Route
-          path="*"
-          element={<h2 style={{ padding: 40 }}>404 - Página no encontrada</h2>}
-        />
+        {}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cargadatos" element={<ExcelManager />} />
+        </Route>
+
+        {}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
