0bd64bfbf00ddcc5a78c05ffec6a14bfd6fd52d2
EfrenAcevedo <joseefrenacevedo3@gmail.com>
2026-04-23 11:14:46 -0600

    cmabios

--- ModuloAsignacionHorarios/src/main.tsx
+++ ModuloAsignacionHorarios/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '../src/Components/style.css';
+import '../src/components/style.css';
 
 import App from './App.tsx'
 
